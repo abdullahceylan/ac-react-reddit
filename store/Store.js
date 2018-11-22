96a2668fbdd5b195d20e3a903faed44a933d063d
Abdullah Ceylan <abdullah@creativouse.co.uk>
2018-11-22 23:34:22 +0000

    Redux logger and dev tools running only in dev mode now.

--- store/Store.js
+++ store/Store.js
@@ -3,15 +3,22 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
 
+const isDev = process.env.NODE_ENV !== 'production';
+
 const loggerMiddleware = createLogger();
-const devToolsCompose = typeof window != 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const devToolsCompose = typeof window != 'undefined' && isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const middlewares = [thunkMiddleware];
+
+if (isDev) {
+  middlewares.push(loggerMiddleware);
+}
 
 const store = createStore(
   rootReducer,
   devToolsCompose(
     applyMiddleware(
-      thunkMiddleware,
-      loggerMiddleware,
+      ...middlewares
     ),
   ),
 );
