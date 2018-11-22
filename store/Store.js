3854b78d8761f68d08468f17fdca7082112f7192
Abdullah Ceylan <abdullah@creativouse.co.uk>
2018-11-22 23:41:18 +0000

    Fixed redux-logger issue on production release.

--- store/Store.js
+++ store/Store.js
@@ -1,18 +1,17 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
 import rootReducer from '../reducers';
 
 const isDev = process.env.NODE_ENV !== 'production';
-
-const loggerMiddleware = createLogger();
-const devToolsCompose = typeof window != 'undefined' && isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const middlewares = [thunkMiddleware];
 
 if (isDev) {
+  const { createLogger } = require('redux-logger');
+  const loggerMiddleware = createLogger();
   middlewares.push(loggerMiddleware);
 }
+
+const devToolsCompose = typeof window != 'undefined' && isDev && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
